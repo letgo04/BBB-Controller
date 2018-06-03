2a494162749f5a2032a6edac78bcbf0b5b881fe3
letgo04 <letgo04@naver.com>
2018-06-03 14:59:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/letgo04/BBB-Controller"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = [Adafruit_BBIO]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
